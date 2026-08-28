ca485b5642169c4e127e6574a729a5d3cf0b6337
陳映臻 <eishin@chenyingzhendeMacBook-Air.local>
2026-08-28 22:14:12 +0800

    fix: 修改根目錄相對路徑

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? 'github.com/EishinCHEN/Resume' : '/',
+  base: '/Resume/',
   plugins: [
     vue(),
     vueDevTools(),
